2001b24f5f7bb735ec378916c61160d15bf6b05a
nunichego <dmitrypuzatov@gmail.com>
2025-03-01 17:15:55 +0100

    add Cells and Path drawer

---
+++ cell.py
@@ -0,0 +1,41 @@
+from graphics import Line, Point
+
+class Cell:
+    def __init__(self, win):
+        self.left_wall = True
+        self.right_wall = True
+        self.top_wall = True
+        self.bottom_wall = True
+        self.__x1 = None
+        self.__x2 = None
+        self.__y1 = None
+        self.__y2 = None
+        self.__win = win
+
+    def draw(self, x1, y1, x2, y2):
+
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+
+        if self.left_wall:
+            line = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(line)
+        if self.right_wall:
+            line = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(line)
+        if self.top_wall:
+            line = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(line)
+        if self.bottom_wall:
+            line = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        center_from = Point((self.__x2 - self.__x1) / 2 + self.__x1, (self.__y2 - self.__y1) / 2 + self.__y1)
+        center_to = Point((to_cell.__x2 - to_cell.__x1) / 2 + to_cell.__x1, (to_cell.__y2 - to_cell.__y1) / 2 + to_cell.__y1)
+        line = Line(center_from, center_to, )
+        if undo == False:
+            self.__win.draw_line(line, "red")
+        else:self.__win.draw_line(line, "gray")
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from graphics import Window, Line, Point
+from graphics import Window
+from cell import Cell
 
 def main():
 
     win = Window(800, 600)
 
-    line_1 = Line(Point(200, 200), Point(400, 400))
-    line_2 = Line(Point(600, 200), Point(700, 400))
-    lines = [line_1, line_2]
-    for line in lines:
-        win.draw_line(line)
+    c = Cell(win)
+    c.left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    y = Cell(win)
+    y.right_wall = False
+    y.draw(125, 125, 200, 200)
+
+    b = Cell(win)
+    b.bottom_wall = False
+    b.draw(225, 225, 250, 250)
+
+    d = Cell(win)
+    d.top_wall = False
+    d.draw(300, 300, 500, 500)
+
+    b.draw_move(d, True)
+    c.draw_move(y, False)
+
+
+
     win.wait_for_close()
 
 if __name__ == "__main__":
